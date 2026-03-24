1de3d2c87ff532a3cece73fa8f78507b19c5bcbf
TahseenLabs <ahmtahseen4@gmail.com>
2026-03-24 19:21:18 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,21 @@ import sys
 
 
 def main():
+    # Displaying the shell prompt ("$ ") without adding a newline
     sys.stdout.write("$ ")
-    pass
+    
+    # Reading a line of input from the user 
+    command = input()
+
+    # Since no actual command execution is implemented yet,
+    # printing a "command not found" message like a real shell
+    print(f"{command}: command not found")
 
 
+# Entry point of the program
+# Ensures main() runs only when the script is executed directly
 if __name__ == "__main__":
     main()
+
+
+    
