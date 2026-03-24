08cdad7d157197274efa7a25fa521ccf6821ae7e
TahseenLabs <ahmtahseen4@gmail.com>
2026-03-24 19:37:51 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,15 +6,13 @@ def main():
     # mimicking how a real shell waits for user commands repeatedly
     while True:
         sys.stdout.write("$ ")
-    while True:
-        sys.stdout.write("$ ")
     
-    # Reading a line of input from the user 
-    command = input()
+        # Reading a line of input from the user 
+        command = input()
 
-    # Since no actual command execution is implemented yet,
-    # printing a "command not found" message like a real shell
-    print(f"{command}: command not found")
+        # Since no actual command execution is implemented yet,
+        # printing a "command not found" message like a real shell
+        print(f"{command}: command not found")
 
 
 # Entry point of the program
